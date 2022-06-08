c3a32ef08c8909e7fbeed6c24aa32d44bfb5407a
Joshua Ham <Joshuaju.ham@gmail.com>
2022-06-07 18:16:48 -0700

    finished basic schema

--- RatingReviewServer/models/index.js
+++ RatingReviewServer/models/index.js
@@ -1,7 +1,7 @@
 const client = require('../db.js');
 
 const getAll = (cb) => {
-  client.query(`SELECT * from test1`, (err, result) => {
+  client.query(`SELECT * from reviews LIMIT 5`, (err, result) => {
     if (err) {
       console.log('error at model')
     } else {
--- schema.sql
+++ schema.sql
@@ -1,10 +1,40 @@
-DROP DATABASE IF EXISTS test WITH (FORCE);
+DROP DATABASE IF EXISTS rr WITH (FORCE);
 
-CREATE DATABASE test;
+CREATE DATABASE rr;
 
-\connect test;
+\connect rr;
 
-CREATE TABLE test1 (
+CREATE TABLE reviews (
   id INTEGER,
-  name VARCHAR(50)
-)
+  product_id INTEGER,
+  rating INTEGER,
+  date BIGINT,
+  summary VARCHAR(1000),
+  body VARCHAR(1000),
+  recommend BOOLEAN,
+  reported BOOLEAN,
+  reviewer_name VARCHAR (60),
+  reviewer_email VARCHAR (60),
+  response VARCHAR,
+  helpfulness INTEGER,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE photos (
+  id INTEGER,
+  review_id INTEGER,
+  url VARCHAR(200)
+);
+
+COPY reviews FROM '/home/joshspc/Desktop/HackReactor/SDCwook/reviews.csv' DELIMITER ',' CSV HEADER;
+
+COPY photos FROM '/home/joshspc/Desktop/HackReactor/SDCwook/reviews_photos.csv' DELIMITER ',' CSV HEADER;
+
+-- reviews v
+-- id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response (null or string),helpfulness
+
+-- phtotos v
+-- id,review_id,url
+
+
+-- /home/joshspc/Desktop/HackReactor/SDCwook/Ratings-and-Reviews/schema.sql
