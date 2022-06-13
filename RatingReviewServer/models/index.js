9f676e59c98afa9b354ca887ea03d881826ff213
Joshua Ham <Joshuaju.ham@gmail.com>
2022-06-13 12:31:08 -0700

    ready for deploy

--- RatingReviewServer/models/index.js
+++ RatingReviewServer/models/index.js
@@ -5,20 +5,20 @@ module.exports = {
 
   getReviews: (cb, values) => {
     client.query (
-      `WITH photos AS (
-        SELECT review_id, jsonb_agg(to_jsonb(photos) - 'review_id') AS photos
-        FROM photos
-        GROUP BY review_id
-        )
-        SELECT reviews.*, photos.photos,
-      CASE WHEN photos.photos is NULL THEN '[]' ELSE photos.photos END AS photos
-      FROM reviews
-      LEFT JOIN photos
-      ON reviews.review_id = photos.review_id
-      WHERE reviews.product_id = $1
-      LIMIT $2
-      `
-      //use order by for sort options
+      // `WITH photos AS (
+      //   SELECT review_id, jsonb_agg(to_jsonb(photos) - 'review_id') AS photos
+      //   FROM photos
+      //   GROUP BY review_id
+      //   )
+      //   SELECT reviews.*, photos.photos,
+      // CASE WHEN photos.photos is NULL THEN '[]' ELSE photos.photos END AS photos
+      // FROM reviews
+      // LEFT JOIN photos
+      // ON reviews.review_id = photos.review_id
+      // WHERE reviews.product_id = $1
+      // LIMIT $2
+      // `
+      'SELECT * FROM reviews WHERE product_id = $1 LIMIT $2'
     , values, (err, result) => {
     if (err) {
       console.log(err, 'error at model')
@@ -97,44 +97,3 @@ module.exports = {
   }
 
 }
-
-// , allrecommends AS (
-//   SELECT
-//   reviews.product_id,
-//   COUNT(reviews.recommend) FILTER (WHERE reviews.recommend = false) as false,
-//   COUNT(reviews.recommend) FILTER (WHERE reviews.recommend = true) as true
-//   FROM reviews
-//   WHERE reviews.product_id = $1
-//   GROUP BY reviews.product_id
-// )
-      // SELECT avgchar.product_id, JSON_CREATE_OBJECT('ratings', allratings.*)
-      // FROM avgchar
-      // JOIN allratings ON reviews.product_id = avgchar.product_id
-      // LIMIT 5
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-json build object (
-  'characteristics',
-  table1 with average size, width, comfort, with maybe to_jsonb
-)
-
-Inside table1:
-  json build object(
-    'Size',
-    table with an id and average value
-  )
-*/
--- schema.sql
+++ schema.sql
@@ -20,8 +20,6 @@ CREATE TABLE reviews (
   PRIMARY KEY (id)
 );
 
--- change default for response as null;
-
 CREATE TABLE photos (
   id INTEGER,
   review_id INTEGER,
@@ -43,14 +41,6 @@ CREATE TABLE revchar (
   value INTEGER,
   PRIMARY KEY (id)
 );
-
--- CREATE TABLE avgchar (
---   id SERIAL,
---   product_id INTEGER,
---   name VARCHAR (10),
---   value INTEGER,
---   PRIMARY KEY (id)
--- );
 
 CREATE TABLE avgchar (
   id SERIAL,
@@ -81,34 +71,18 @@ CREATE TABLE allproductid (
 ALTER TABLE reviews
   RENAME COLUMN id TO review_id;
 
--- create index review_id ON reviews(review_id);
--- create index product_id ON reviews(product_id);
--- create index name ON char(name);
--- create index product_id ON char(product_id);
--- create index product_id ON allproductid(product_id);
-
--- ALTER TABLE reviews
---   ADD  photos text[] DEFAULT array[]::varchar[];
-
--- INSERT INTO avgchar (product_id, name, value)
---   SELECT char.product_id, char.name, AVG(revchar.value)
---   FROM revchar JOIN char
---   ON revchar.characteristic_id = char.id
---   WHERE char.name = 'Fit'
---   GROUP BY char.product_id, char.name;
-
--- with fittable as (
---   SELECT allproductid.productid, char.name, AVG(revchar.value) FILTER (WHERE char.name = 'Fit') AS value
---   FROM allproductid
---   JOIN
---   )
+create index review_review_id ON reviews(review_id);
+create index product_product_id ON reviews(product_id);
+create index name ON char(name);
+create index char_product_id ON char(product_id);
+create index allrpductid_product_id ON allproductid(product_id);
+create index photos_review_id ON photos(review_id);
 
 
 INSERT INTO allproductid (product_id)
 SELECT DISTINCT reviews.product_id
 FROM reviews;
 
--- SELECT allproductid.product_id, AVG(revchar.value) AS value FROM revchar JOIN char ON revchar.characteristic_id = char.id JOIN allproductid ON allproductid.product_id = char.product_id WHERE char.name = 'Fit' GROUP BY allproductid.product_id LIMIT 5;
 
 WITH fittable AS (
   SELECT allproductid.id, allproductid.product_id, AVG(revchar.value) AS fitvalue
@@ -154,19 +128,13 @@ WITH fittable AS (
     GROUP BY allproductid.product_id , allproductid.id
 ), averagetable AS (
   SELECT allproductid.id,
-   allproductid.product_id,
-  --  JSON_BUILD_OBJECT('id', allproductid.id, 'value', fittable.fitvalue) AS Fit,
-  --  JSON_BUILD_OBJECT('id', allproductid.id, 'value', lengthtable.lengthvalue) AS Length,
-  --  JSON_BUILD_OBJECT('id', allproductid.id, 'value', comforttable.comfortvalue) AS Comfort,
-  --  JSON_BUILD_OBJECT('id', allproductid.id, 'value', qualitytable.qualityvalue) AS Quality,
-  --  JSON_BUILD_OBJECT('id', allproductid.id, 'value', widthtable.widthvalue) AS Width,
-  --  JSON_BUILD_OBJECT('id', allproductid.id, 'value', sizetable.sizevalue) AS Size
-  fittable.fitvalue AS Fit,
-  lengthtable.lengthvalue AS Length,
-  comforttable.comfortvalue AS Comfort,
-  qualitytable.qualityvalue AS Quality,
-  widthtable.widthvalue AS Width,
-  sizetable.sizevalue AS Size
+    allproductid.product_id,
+    fittable.fitvalue AS Fit,
+    lengthtable.lengthvalue AS Length,
+    comforttable.comfortvalue AS Comfort,
+    qualitytable.qualityvalue AS Quality,
+    widthtable.widthvalue AS Width,
+    sizetable.sizevalue AS Size
     FROM allproductid
     LEFT JOIN fittable
     ON allproductid.product_id = fittable.product_id
@@ -185,81 +153,17 @@ INSERT INTO avgchar (product_id, fit, length, comfort, quality, width, size)
 SELECT averagetable.product_id, averagetable.Fit, averagetable.Length, averagetable.Comfort, averagetable.Quality, averagetable.Width, averagetable.Size
 FROM averagetable;
 
--- to_jsonb(averagetable.*) - 'id' - 'product_id'
+create index avgchar_product_id ON avgchar(product_id);
 
+ALTER TABLE reviews
+  ADD photos jsonb default '[]'::jsonb;
 
-
--- WITH allratings AS (
---   SELECT
---   AVG(reviews.rating) FILTER (WHERE reviews.rating = 1) AS one
---   FROM reviews
---   WHERE reviews.product_id = 1
--- )
--- SELECT * FROM allratings LIMIT 5;
-
-
-  --  lengthtable.lengthvalue,
-  --  comforttable.comfortvalue,
-  --  qualitytable.qualityvalue,
-  --  widthtable.widthvalue,
-  --  sizetable.sizevalue
-
--- CREATE TABLE avgchar (
---   id SERIAL,
---   product_id INTEGER,
---   characteristics JSON,
---   PRIMARY KEY (id)
--- );
-
-
--- CREATE TABLE test (W
---   id serial,
---   name VARCHAR(10),
---   PRIMARY KEY (id)
--- );
-
--- CREATE TABLE testtwo (
---   id serial,
---   name VARCHAR(10),
---   city VARCHAR(10),
---   PRIMARY KEY (id)
--- );
-
--- INSERT INTO test (name) VALUES ('bob'), ('harry'), ('me');
-
--- INSERT INTO testtwo (name, city) VALUES ('bob', 'sf'), ('harry', 'sd'), ('me', 'la'), ('bob', 'sj');
-
--- SELECT test.name FROM test JOIN testtwo ON test.name = testtwo.name;
-
-
-
--- CREATE TABLE ratings (
---   id SERIAL,
---   product_id INTEGER,
---   five INTEGER,
---   four INTEGER,
---   three INTEGER,
---   two INTEGER,
---   one INTEGER,
---   PRIMARY KEY (id)
--- );
-
-
-
-
--- reviews v
--- id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response (null or string),helpfulness
-
--- phtotos v
--- id,review_id,url
-
-
--- /home/joshspc/Desktop/HackReactor/SDCwook/Ratings-and-Reviews/schema.sql
-
--- char_review
--- id,characteristic_id,review_id,value
--- 19327575,3347679,5774952,5
-
--- char
--- id,product_id,name
--- 3347679,1000011,"Quality"
+WITH photos AS (
+  SELECT review_id, jsonb_agg(to_jsonb(photos) - 'review_id') AS photos
+  FROM photos
+  GROUP BY review_id
+  )
+  UPDATE reviews
+  SET photos = CASE WHEN photos.photos IS NULL THEN '[]' ELSE photos.photos END
+  FROM photos
+  WHERE reviews.review_id = photos.review_id
